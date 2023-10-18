2fd487195c5c11d9284a6b0c9594d5596e0cf6f8
UX403-A06 UX403-A06 <P002014>
2023-10-18 08:14:37 +0000

    Todo hasta 18/10

---
+++ searchfilter.student06/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("searchfilter.student06.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
